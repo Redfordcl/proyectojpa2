11e6dd80f3fc571e1e8768e56babcba6e0307b0e
Redfordcl <46832488+Redfordcl@users.noreply.github.com>
2019-01-18 22:01:59 -0500

    Add files via upload

---
+++ jpa2/dataventa.sql
@@ -0,0 +1,104 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.2.20-MariaDB : Database - venta
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`venta` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `venta`;
+
+/*Table structure for table `categoria` */
+
+DROP TABLE IF EXISTS `categoria`;
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `denominacion` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `categoria` */
+
+insert  into `categoria`(`id`,`denominacion`) values (1,'bebiba a'),(2,'bebiba b');
+
+/*Table structure for table `cliente` */
+
+DROP TABLE IF EXISTS `cliente`;
+
+CREATE TABLE `cliente` (
+  `idcliente` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(120) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `direccion` varchar(160) NOT NULL,
+  `dni` char(8) NOT NULL,
+  PRIMARY KEY (`idcliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Data for the table `cliente` */
+
+insert  into `cliente`(`idcliente`,`nombre`,`apellido`,`direccion`,`dni`) values (7,'elmer','blas','huaraz','12345678'),(8,'elmer1','blas','huaraz','12345678');
+
+/*Table structure for table `detalle` */
+
+DROP TABLE IF EXISTS `detalle`;
+
+CREATE TABLE `detalle` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `precio` decimal(10,2) unsigned NOT NULL,
+  `cantidad` int(11) unsigned NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `factura_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_detalle_producto1_idx` (`producto_id`),
+  KEY `fk_detalle_factura1_idx` (`factura_id`),
+  CONSTRAINT `fk_detalle_factura1_idx` FOREIGN KEY (`factura_id`) REFERENCES `factura` (`id`),
+  CONSTRAINT `fk_detalle_producto1_idx` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8;
+
+/*Data for the table `detalle` */
+
+/*Table structure for table `factura` */
+
+DROP TABLE IF EXISTS `factura`;
+
+CREATE TABLE `factura` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `numero` int(7) unsigned zerofill NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `cliente_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_factura_cliente1_idx` (`cliente_id`),
+  CONSTRAINT `fk_factura_cliente1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`idcliente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+/*Data for the table `factura` */
+
+/*Table structure for table `producto` */
+
+DROP TABLE IF EXISTS `producto`;
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `stock` int(11) NOT NULL,
+  `categoria_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+/*Data for the table `producto` */
+
+insert  into `producto`(`id`,`nombre`,`stock`,`categoria_id`) values (9,'Cerveza cuzqueña',180,1),(10,'Cerveza pilsen',200,1),(11,'Cerveza cristal',100,2),(12,'Cerveza corona',130,2);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
